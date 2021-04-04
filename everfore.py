30ca06f3f74778737b7f3c162c626ea29db4c180
Cubenir <74281446+Cubenir@users.noreply.github.com>
2021-04-03 22:42:56 -0600

    Create everfore.py

---
+++ everfore.py
@@ -0,0 +1,20 @@
+loop = 1
+while loop == 1:
+	if loop == 1:
+		print("- XXXXXX- - - - - - - - - - - - -XXXXXX - - - - - - - - - - - ")
+		print("- XXXXX - - - - - - - - - - - - -XXXXX- - - - - - - - - - - - ")
+		print("- XX- - - - - - - - - - - - - - -XX - - - - - - - - - - - - - ")
+		print("- XX- - - - - - - - - - - - - - -XX - - - - - - - - - - - - - ")
+		print("- XX- - - - - - - - - - - - - - -XX - - - - - - - - - - - - - ")
+		print("- XXXX- - - - - - - - - - - - - -XXXX - - - - - - - - - - - - ")
+		print("- XXXXX -XX - XX- -XXX- -XX XXX -XXXXX- - - - -XX XXX - - - - ")
+		print("- XXXX- -XX - XX- XXXXX -XXXXXX -XXXX - -XXX- -XXXXXX - XXXX- ")
+		print("- XX- - -XX - XX- XX- X -XXX- - -XX - - XXXXX -XXX- - -XXXXXX ")
+		print("- XX- - -XX - XX- XXXX- -XX - - -XX - - XX-XX -XX - - -XXX- X ")
+		print("- XX- - -XX - XX- XX- - -XX - - -XX - - XX-XX -XX - - -XXXX - ")
+		print("- XXXXX - XX-XX - XXXXX -XX - - -XX - - XX-XX -XX - - -XXXXXX ")
+		print("- XXXXXX- -XXX- - -XXXX -XX - - -XX - - -XXX- -XX - - - XXXX- ")
+		print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+		 
+
+
